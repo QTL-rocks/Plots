576d7dadfc2e60834a37c11591ed9a6e1e44f542
Hao Cheng <reworkhow@gmail.com>
2020-10-21 21:59:32 -0700

    Update distributions.r

--- distributions.r
+++ distributions.r
@@ -6,4 +6,6 @@ library(nimble)
 curve(expr = ddexp(x), from = -4, to = 4, col = "red", lwd = 3.5,ylab="",xlab="",xaxt='n',yaxt='n')
 
 library(EnvStats)
-curve(expr =   dnormMix(x, mean1 = 0, sd1 = 1, mean2 = 0, sd2 = 0.0000000001, p.mix = 0.5), from = -4, to = 4, col = "red", lwd = 3.5,ylab="",xlab="",xaxt='n',yaxt='n')
+curve(expr =   dnormMix(x, mean1 = 0, sd1 = 10, mean2 = 0, sd2 = 1, p.mix = 0.5), from = -4, to = 20, col = "red", lwd = 3.5,ylab="",xlab="",xaxt='n',yaxt='n')
+
+curve(expr =   dnormMix(x, mean1 = 0, sd1 = 10, mean2 = 0, sd2 = 1, mean3 = 0, sd3 = 1, p.mix = 0.5), from = -4, to = 20, col = "red", lwd = 3.5,ylab="",xlab="",xaxt='n',yaxt='n')
